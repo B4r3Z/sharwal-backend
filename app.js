fb410dfefbceca81c9a3c85dba90b352ee9eed3f
B4r3Z <barezdilshad@gmail.com>
2023-05-06 05:08:00 +0300

    Add user model

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 import express from "express";
-import dotenv from "dotenv";
+import { ErrorHandler } from "./utils/ErrorHandler.js";
 export const app = express();
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import fileUpload from "express-fileupload";
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
 
 dotenv.config({ path: "./config/.env" });
+
+app.use(ErrorHandler);
+
+//ErrorHandling
