4f6f4a627416025cd9902b2557b73c39b080fc02
B4r3Z <barezdilshad@gmail.com>
2023-05-28 00:33:17 +0300

    add controller for adding new user add route for adding new user add jwt token add send mail

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ export const app = express();
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
-import userController from "./controller/userController.js";
+import userRoutes from "./routes/users.routes.js";
 import dotenv from "dotenv";
 
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config({ path: "./config/.env" });
 
-app.use("/api/v2/user", userController);
+app.use("/api/v2/user", userRoutes);
 app.use(ErrorHandler);
 
 //ErrorHandling
--- controller/userController.js
+++ controller/userController.js
@@ -1,28 +1,92 @@
-import express from "express";
+
 import path from "path";
 import User from "../model/userModel.js";
-import { upload } from "../multer.js";
 import { ErrorHandler } from "../utils/ErrorHandler.js";
+import fs from "fs";
+import { sendMail } from "../utils/sendMail.js";
+import jwt from "jsonwebtoken";
+import {catchAsyncErrors} from "../middleware/catchAsyncErrors.js";
 
-const router = express.Router();
-export default router;
+export const createUser =  async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
 
-router.post("/create-user", upload.single("file"), async (req, res) => {
-  const { name, email, password } = req.body;
-  const userEmail = await User.findOne({ email });
+    if (userEmail) {
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        }
+      });
+      return next(new ErrorHandler("User already exists", 400));
+    }
 
-  if (userEmail) {
-    return next(new ErrorHandler("Email already exists", 400));
+    const filename = req.file.filename;
+    const fileUrl = path.join(filename);
+
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      avatar: fileUrl,
+    };
+
+    const activationToken = createActivationToken(user);
+
+    const activationUrl = `https://eshop-tutorial-cefl.vercel.app/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email: user.email,
+        subject: "Account Activation Link",
+        message: `Hello ${user.name},\n\nPlease activate your account by clicking the link below:\n\n${activationUrl}`,
+      });
+      res.status(201).json({
+        success: true,
+        message: `Success! Activation sent to ${user.email}`,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 400));
   }
+};
 
-  const fileName = req.file.filename;
-  const fileUrl = path.join(fileName);
 
-  const user = await User.create({
-    name: name,
-    email: email,
-    password: password,
-    avatar: fileUrl,
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_TOKEN_SECRET, {
+    expiresIn: "5m",
   });
-  console.log(user);
+};
+
+export const activateEmail = catchAsyncErrors(async(req, res, next) => {
+try {
+  const { activation_token } = req.body;
+  const newUser = jwt.verify( 
+    activation_token,
+    process.env.ACTIVATION_TOKEN_SECRET);
+
+    if(!newUser) {
+      return next(new ErrorHandler('Invalid Token', 400));
+    }
+    const { name, email, password, avatar } = newUser;
+
+      User.create({ 
+        name,
+        email,
+        password,
+        avatar
+      });
+
+      sendToken(newUser, 201, res);
+
+ 
+} catch (error) {
+  return next(new ErrorHandler(error.message, 500));
+}
 });
+
